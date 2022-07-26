ab80e7eaddde2453fca4157dca10ebcfe7a5ea53
Fahdi26 <109886904+Fahdi26@users.noreply.github.com>
2022-07-26 23:54:27 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,91 +1,16 @@
-let { MessageType } = (await import('@adiwajshing/baileys')).default
+let handler = async (m, { conn }) => {
+/*
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  const town = await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m, {quoted: fkontak})
+  await conn.reply(m.chat, "My Owner", m, {quoted: town})
+  */
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6289524208209:+62 895-2420-8209\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih owner ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, {quoted: m})
 
-let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-  let type = (args[0] || '').toLowerCase()
-  let _type = (args[0] || '').toLowerCase()
-
-//------- NOMOR
-  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
-  let teksnomor = `${htki} *OWNER* ${htka}
-✦ @${nomorown.split`@`[0]} ✦
-------- ${nameown} -------
-
-📮 *Note:*
-• Owner tidak menerima save contact
-• Owner berhak blockir tanpa alasan
-• Berbicaralah yang sopan & tidak spam
-• Owner Hanya merespon yang berkaitan dengan BOT
-• No Telp`
-
-//------------ BIO
-let ppown = 'https://telegra.ph/file/02a2903c1e25228285740.jpg'
-let teksbio = `${htki} *BIODATA* ${htka}
-❑ *Nama* :Bara
-❑ *Umur* :13 Tahun
-❑ *Kelas* :1 Smp
-
-❑ *Ulang Tahun* :18 Okto 2008
-❑ *Alamat* : Indonesia,Jawa Barat, Bandung
-
-${htjava} *Instagram:* ${sig}
-${htjava} *Github:* ${sgh}
-•·––––––––––––––––––––––––––·•
-`
-  let teks = 'P - Pilih dibawah kak'
-const sections = [
-   {
-	title: `${htjava} OWNER –––––––––·•`,
-	rows: [
-	    {title: "📱 • Nomor", rowId: ".owner nomor"},
-	{title: "🌎 • Script", rowId: ".sc"},
-	]
-    },{
-	title: `${htjava} SUPPORT ME –––––––·•`,
-	rows: [
-	    {title: "💹 • Donasi", rowId: ".donasi"},
-	{title: "🔖 • Sewa", rowId: ".sewa"},
-	]
-  },
-]
-
-const listMessage = {
-  text: teks,
-  footer: null,
-  title: `${htki} *OWNER* ${htka}`,
-  buttonText: "Click Here !",
-  sections
 }
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
 
-  try {
-    if (/(creator|owner)/i.test(command)) {
-      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
-        switch (type) {
-          case 'nomor':
-          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
-            break
-            
-          default:
-            return await conn.sendMessage(m.chat, listMessage, m, { contextInfo: { mentionedJid: [m.sender] }})
-        }
-    } else if (/enchant|enchan/i.test(command)) {
-      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
-      switch (_type) {
-        case 't':
-          break
-        case '':
-          break
-
-        default:
-          return conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
-      }
-    }
-  } catch (err) {
-    m.reply("Error\n\n\n" + err.stack)
-  }
-}
-
-handler.help = ['owner', 'creaor']
-handler.tags = ['main', 'info']
-handler.command = /^(owner|creator)/i
+handler.command = /^(owner|creator)$/i
 
 export default handler
